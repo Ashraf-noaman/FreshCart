457320e3d0585841cb6f2b4f68e6ea76f9e396ba
Ashraf Noaman <ashrafnoaman17@gmail.com>
2026-03-08 17:13:40 +0200

    feat: initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <>
-    Home
+    <h2>Home Page</h2>
+      <Button variant="default">Button</Button>
+      <h2>...</h2>
     </>
   );
 }
